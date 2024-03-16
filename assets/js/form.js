2fa119cdca554225520a0cb3991dada9d628bda7
rvanetta97 <rachel.vanetta@gmail.com>
2024-03-16 19:06:26 -0400

    Form creation/initial HTML

---
+++ assets/js/form.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("form")
+const uname = document.getElementById("uname")
+const blogtitle = document.getElementById("blogtitle")
+const bcontent = document.getElementById(bcontent)
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const username = uname.value 
+    const title = blogtitle.value 
+    const blogpost = bcontent.value
+
+    localStorage.setItem("username", uname.value);
+    localStorage.setItem("title", blogtitle.value);
+    localStorage.setItem("blog-post", bcontent.value);
+
+    window.location.href=""
+})
