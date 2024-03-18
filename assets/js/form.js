1926025f61ae500ee949a6ab62164b2b07c81e12
rvanetta97 <rachel.vanetta@gmail.com>
2024-03-18 10:43:11 -0400

    changes to form submission data

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,20 +4,31 @@ checkbox.addEventListener("change", () => {
 })
 
 const form = document.getElementById("form").value
-const uname = document.getElementById("uname").value
-const blogtitle = document.getElementById("blogtitle").value
-const bcontent = document.getElementById("bcontent").value
+
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    const username = uname.value 
+    const uname = document.getElementById("uname").value
+    const blogtitle = document.getElementById("blogtitle").value
+    const bcontent = document.getElementById("bcontent").value
+    
+    const blogPost = {
+      author: uname,
+      title: blogtitle,
+      Content: bcontent
+    };
+    
+    const blogPostJSON = JSON.stringify(blogPost);
+
+    localStorage.setItem('blogPost', blogPostJSON);
+    /*const username = uname.value 
     const title = blogtitle.value 
     const blogpost = bcontent.value
 
     localStorage.setItem("username", uname.value);
     localStorage.setItem("title", blogtitle.value);
-    localStorage.setItem("blogpost", bcontent.value);
+    localStorage.setItem("blogpost", bcontent.value);*/
 
-    window.location.href= "https://rvanetta97.github.io/blog_posting/blog.html";
-    return
+    window.location.href= 'https://rvanetta97.github.io/blog_posting/blog.html';
+    /*return*/
 })
