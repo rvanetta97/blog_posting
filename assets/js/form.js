933f9dd4033d04e3f122731ddd39945fcd24c01b
rvanetta97 <rachel.vanetta@gmail.com>
2024-03-18 12:15:57 -0400

    changes to redirecting page

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,33 +3,26 @@ checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
 
-document.getElementById('form').addEventListener("submit", function(e) {
-    e.preventDefault();
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
     const uname = document.getElementById("uname").value
     const blogtitle = document.getElementById("blogtitle").value
     const bcontent = document.getElementById("bcontent").value
     
-    let blogPosts = JSON.parse(localstorage.getItem('blogPosts')) || []
-
+    let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
     const blogPost = {
-      author: uname,
-      title: blogtitle,
-      Content: bcontent
+        author: uname,
+        title: blogtitle,
+        Content: bcontent
     };
-    
-    blogPosts.push(blogPost)
-
-    const blogPostJSON = JSON.stringify(blogPost);
-
-    localStorage.setItem('blogPost', JSON.stringify(blogPosts));
-    /*const username = uname.value 
-    const title = blogtitle.value 
-    const blogpost = bcontent.value
-
-    localStorage.setItem("username", uname.value);
-    localStorage.setItem("title", blogtitle.value);
-    localStorage.setItem("blogpost", bcontent.value);*/
-
+    console.log(blogPost)
+    blogPosts.push(blogPost);
+    const blogPostsJSON = JSON.stringify(blogPosts);
+  
+/*redirects user to the new page*/
     window.location.href= 'https://rvanetta97.github.io/blog_posting/blog.html';
-    /*return*/
-})
+
+    return
+});
