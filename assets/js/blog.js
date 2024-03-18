8d26a520df7da4834b1af7aab43354094a581d02
rvanetta97 <rachel.vanetta@gmail.com>
2024-03-18 17:17:23 -0400

    building blog website

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,10 +1,45 @@
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
+    document.body.classList.toggle("dark")
 })
 
+
 window.addEventListener('load', () => {
-    const username = localStorage.getItem('uname')
-    const title = localStorage.getItem('blogtitle')
-    const blogpost= localStorage.getItem("blogpost")
-})
+    const displayPosts = function (blogPosts) {
+        const blogPosts = JSON.parse(localStorage.getItem("blogPosts"));
+        const submittedpost = document.getElementById('submittedpost');
+        submittedpost.innerHTML = '';
+
+        if (Array.isArray(blogPosts)) {
+            blogPosts.forEach(blogPost => {
+                const post = document.createElement('div');
+
+                const title = document.createElement('span');
+                title.textContent = blogPost.title;
+                post.appendChild(title);
+
+                const content = document.createElement('span');
+                content.textContent = blogPost.text;
+                post.appendChild(content);
+
+                const author = document.createElement('span');
+                author.textContent = blogPost.authorName;
+                post.appendChild(author);
+
+                submittedpost.appendChild(post);
+            });
+        }
+    };
+});
+
+        /*object.entries(blogPost).foreach(([key, value])); {
+            const keyValueElement = document.createElement('span')
+            keyValueElement.textContent = '${key}: ${value}';
+            objectElement.appendChild(keyValueElement);
+        }
+        submittedpost.appendChild(objectElement)*/
+    });
+       
+    
+   
+});
